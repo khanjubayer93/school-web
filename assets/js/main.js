ac8ebc1b218e0250893ea9722b8c29db4aec51c4
jubayer <jubayerkhan711@gmail.com>
2024-08-26 11:55:44 +0600

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+(function ($) {
+  ("use strict");
+
+  // $(".banner-slider").slick({
+  //   infinite: true,
+  //   slidesToShow: 1,
+  //   slidesToScroll: 1,
+  //   autoplay: true,
+  //   autoplaySpeed: 2000,
+  // });
+})(jQuery);
