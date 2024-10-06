027a2a51488c685380e2df11458ebd4cb8c314b2
jubayer <jubayerkhan711@gmail.com>
2024-10-06 08:19:21 +0600

    up

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,21 @@
 (function ($) {
   ("use strict");
 
-  // $(".banner-slider").slick({
-  //   infinite: true,
-  //   slidesToShow: 1,
-  //   slidesToScroll: 1,
-  //   autoplay: true,
-  //   autoplaySpeed: 2000,
-  // });
+  $(".jk-banner-wrap").slick({
+    dots: false,
+    infinite: true,
+    speed: 700,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    cssEase: "linear",
+  });
+
+  $(".popular-slider").slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+  });
 })(jQuery);
